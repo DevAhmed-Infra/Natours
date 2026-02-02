eac82a146dbf5320717c09533b3c455e80d7832d
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-02-02 20:48:00 +0200

    Adding field limiting and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,21 +6,46 @@ const httpStatus = require("../utils/httpStatus");
 const getAllTours = asyncHandler(async (req, res) => {
   const queryObj = { ...req.query };
 
+  // filtering
+  const excludedFields = ["sort", "page", "limit", "fields"];
+  excludedFields.forEach((field) => delete queryObj[field]);
+
+  // Advanced filreting
   const mongoQuery = {};
 
   Object.keys(queryObj).forEach((key) => {
     const operatorMatch = key.match(/(.+)\[(gte|gt|lte|lt)\]/);
     if (operatorMatch) {
-      const field = operatorMatch[1]; 
-      const operator = operatorMatch[2]; 
+      const field = operatorMatch[1];
+      const operator = operatorMatch[2];
       mongoQuery[field] = { [`$${operator}`]: Number(queryObj[key]) };
     } else {
       mongoQuery[key] = queryObj[key];
     }
   });
 
+  let query = Tour.find(mongoQuery);
 
-  const tours = await Tour.find(mongoQuery);
+  // sorting
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    console.log(sortBy);
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // field limiting
+
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    query = query.select(fields);
+  } else {
+    query = query.select("-__v");
+  }
+
+  const tours = await query;
 
   res.status(200).json({
     status: httpStatus.SUCCESS,
