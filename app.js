a5ff0b8b7a54dac597ae26c7dff8881e730f5040
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-02-04 19:05:54 +0200

    Improving Error Handling

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const httpStatus = require("../Natours/utils/httpStatus");
+const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1) MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -15,22 +15,23 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
 app.use((err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
-    status: httpStatus.ERROR,
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || httpStatus.ERROR;
+
+  res.status(err.statusCode).json({
+    status: err.status,
     message: err.message,
   });
 });
