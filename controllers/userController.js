841ed84cf1bb441fb44428aefe4fb8f6497e684b
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-02-07 23:41:02 +0200

    adding update logged in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,10 @@ const httpStatus = require("../utils/httpStatus");
 const AppError = require("../utils/appError");
 const APIFeatures = require("../utils/apiFeatures");
 const User = require("../models/userModel");
+const filterObj = require('../utils/filterObj')
+
+
+
 
 const getAllUsers = asyncHandler(async (req, res, next) => {
   const features = new APIFeatures(User.find(), req.query)
@@ -29,6 +33,36 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
     })
 });
 
+
+const updateMe = asyncHandler(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 const deleteMe = asyncHandler(async (req, res, next) => {
   const id = req.user.id;
   await User.findByIdAndUpdate(id, { active: false });
@@ -42,7 +76,8 @@ const deleteMe = asyncHandler(async (req, res, next) => {
 
 module.exports = {
   getAllUsers,
-  deleteMe
+  deleteMe,
+  updateMe,
 }
 
 
