022e1116d20171172930d86eff989465caf9ee84
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-02-08 19:23:42 +0200

    refactoring auth  controller

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitizer = require("express-mongo-sanitize");
+const { xss } = require("express-xss-sanitizer");
+const hpp = require("hpp");
+
 const AppError = require("./utils/appError");
-const globalErrorHandler = require('./controllers/errorController');
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -11,7 +17,30 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 100,
+  message: "Too many requests",
+});
+
+app.use("/api", limiter);
+app.use(helmet());
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitizer());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
